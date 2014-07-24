a5de2a32f00c3f20e5d838dd386f308111cdaf19
Samuel A. Lamb <samuei@ku.edu>
2014-07-24 16:42:47 -0500

    Update script.py
    Should be the final version.
    
    * Added what should function as docstrings.
    * Hard-coded away long bases. After some thought, they didn't make any sense to me.
    * Longer limb strings are now spaced correctly, but only for odd-length strings. This is directly tied to the base width (see comment/thread).

--- Pretty-Trees/script.py
+++ Pretty-Trees/script.py
@@ -1,17 +1,30 @@
 # http://www.reddit.com/r/dailyprogrammer/comments/1t0r09/121613_challenge_145_easy_tree_generation/
 
 class PrettyTree(object):
+    """ An object for building pretty trees """
     WIDTH_OF_BASE = 3
+    # The base's width is constant due to project specs.
+    
     def __init__(self, width, base, limbs):
-# This bit sanitizes the width input. Even trees are more work. Can't have that.
+        """
+        Width = Width of bottom row of tree limbs.
+        Base = Character that the tree's trunk will be made of (single character only).
+        Limbs = String that the tree's limbs will be made of (longer strings allowed).
+        Note: Even-length limb strings will result in an ugly tree. I won't stop you,
+              but you're going to have to live with your ugly tree, Charlie Brown.
+        """
+        # This bit sanitizes the width input. Even trees are ugly. Can't have that.
         self.width = abs(width)
         while (self.width%2 == 0):
-            print "I'm sorry. The program is a little naive. Please enter an odd number:"
-            self.width = abs(int(raw_input()))
+            self.width = abs(int(raw_input("I'm sorry. The program is a little naive. Please enter an odd number:")))
         self.base = base
+        # This bit sanitizes the base input.
+        while (len(self.base) > 1):
+            self.base = raw_input("I'm sorry, but the base can't have more than one character. Please enter a single character:")
         self.limbs = limbs
-# As the name suggests, this draws the tree, as specified.
+    
     def draw_Tree(self):
+        """ This prints the pretty tree """
         measure = 1
         height = (self.width + 1)/2 + 1
         # Drawing the limbs:
@@ -19,8 +32,4 @@ class PrettyTree(object):
             print ((height - i)*len(self.limbs))*" "+(self.limbs * measure)
             measure += 2
         # Drawing the base:
-        print " "*((height - 1)*len(self.limbs))+self.base*self.WIDTH_OF_BASE
-
-# Note: You can input strings, rather than characters for base and limbs. Currently, short
-# strings for limbs are more or less supported, but I ran out of time before exactly fixing
-# the spacing problem. Strings as the base just get all kinds of ugly. Don't do that.
+        print " "*(((height - 1)*len(self.limbs))+((len(self.limbs)-self.WIDTH_OF_BASE)/2))+self.base*self.WIDTH_OF_BASE
